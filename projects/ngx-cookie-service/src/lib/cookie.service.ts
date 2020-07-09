778548331ed55b85ba96d902f8a6ecc4288654e7
rojedalopez <rojedalopez@gmail.com>
2020-07-09 09:37:50 -0500

    Update cookie.service.ts
    fixed lint alert

--- projects/ngx-cookie-service/src/lib/cookie.service.ts
+++ projects/ngx-cookie-service/src/lib/cookie.service.ts
@@ -154,12 +154,14 @@ export class CookieService {
    * @param path   Cookie path
    * @param domain Cookie domain
    */
-  delete(name: string, 
+  delete(
+          name: string, 
           path?: string, 
           domain?: string, 
           secure?: boolean, 
           httpOnly?: boolean, 
-          sameSite: 'Lax' | 'None' | 'Strict' = 'Lax'): void {
+          sameSite: 'Lax' | 'None' | 'Strict' = 'Lax'
+  ): void {
     if (!this.documentIsAccessible) {
       return;
     }
