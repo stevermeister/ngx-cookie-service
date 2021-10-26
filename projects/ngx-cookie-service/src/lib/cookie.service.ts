4bc44bff8bfec62a708cb9bc86bbbe1a8fe03724
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2021-10-25 23:44:47 -0400

    docs: updates methods comments

--- projects/ngx-cookie-service/src/lib/cookie.service.ts
+++ projects/ngx-cookie-service/src/lib/cookie.service.ts
@@ -14,7 +14,7 @@ export class CookieService {
   constructor(
     @Inject(DOCUMENT) private document: any,
     // Get the `PLATFORM_ID` so we can check if we're in a browser.
-    @Inject(PLATFORM_ID) private platformId
+    @Inject(PLATFORM_ID) private platformId,
   ) {
     this.documentIsAccessible = isPlatformBrowser(this.platformId);
   }
@@ -24,6 +24,9 @@ export class CookieService {
    *
    * @param name Cookie name
    * @returns property RegExp
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   private static getCookieRegExp(name: string): RegExp {
     const escapedName: string = name.replace(/([\[\]\{\}\(\)\|\=\;\+\?\,\.\*\^\$])/gi, '\\$1');
@@ -38,7 +41,7 @@ export class CookieService {
    *
    * @returns The unencoded version of an encoded component of a Uniform Resource Identifier (URI).
    *
-   * @author: Pavan Kumar Jadda
+   * @author: Stepan Suvorov
    * @since: 1.0.0
    */
   private static safeDecodeURIComponent(encodedURIComponent: string): string {
@@ -55,6 +58,9 @@ export class CookieService {
    *
    * @param name Cookie name
    * @returns boolean - whether cookie with specified name exists
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   check(name: string): boolean {
     if (!this.documentIsAccessible) {
@@ -70,6 +76,9 @@ export class CookieService {
    *
    * @param name Cookie name
    * @returns property value
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   get(name: string): string {
     if (this.documentIsAccessible && this.check(name)) {
@@ -88,6 +97,9 @@ export class CookieService {
    * Get all cookies in JSON format
    *
    * @returns all the cookies in json
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   getAll(): { [key: string]: string } {
     if (!this.documentIsAccessible) {
@@ -117,6 +129,9 @@ export class CookieService {
    * @param domain   Cookie domain
    * @param secure   Secure flag
    * @param sameSite OWASP samesite token `Lax`, `None`, or `Strict`. Defaults to `Lax`
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   set(name: string, value: string, expires?: number | Date, path?: string, domain?: string, secure?: boolean, sameSite?: 'Lax' | 'None' | 'Strict'): void;
 
@@ -135,6 +150,9 @@ export class CookieService {
    * @param name     Cookie name
    * @param value    Cookie value
    * @param options  Body with cookie's params
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   set(
     name: string,
@@ -145,7 +163,7 @@ export class CookieService {
       domain?: string;
       secure?: boolean;
       sameSite?: 'Lax' | 'None' | 'Strict';
-    }
+    },
   ): void;
 
   set(
@@ -155,7 +173,7 @@ export class CookieService {
     path?: string,
     domain?: string,
     secure?: boolean,
-    sameSite?: 'Lax' | 'None' | 'Strict'
+    sameSite?: 'Lax' | 'None' | 'Strict',
   ): void {
     if (!this.documentIsAccessible) {
       return;
@@ -200,7 +218,7 @@ export class CookieService {
       options.secure = true;
       console.warn(
         `[ngx-cookie-service] Cookie ${name} was forced with secure flag because sameSite=None.` +
-          `More details : https://github.com/stevermeister/ngx-cookie-service/issues/86#issuecomment-597720130`
+        `More details : https://github.com/stevermeister/ngx-cookie-service/issues/86#issuecomment-597720130`,
       );
     }
     if (options.secure) {
@@ -224,6 +242,9 @@ export class CookieService {
    * @param domain Cookie domain
    * @param secure Cookie secure flag
    * @param sameSite Cookie sameSite flag - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   delete(name: string, path?: string, domain?: string, secure?: boolean, sameSite: 'Lax' | 'None' | 'Strict' = 'Lax'): void {
     if (!this.documentIsAccessible) {
@@ -240,6 +261,9 @@ export class CookieService {
    * @param domain Cookie domain
    * @param secure Is the Cookie secure
    * @param sameSite Is the cookie same site
+   *
+   * @author: Stepan Suvorov
+   * @since: 1.0.0
    */
   deleteAll(path?: string, domain?: string, secure?: boolean, sameSite: 'Lax' | 'None' | 'Strict' = 'Lax'): void {
     if (!this.documentIsAccessible) {
