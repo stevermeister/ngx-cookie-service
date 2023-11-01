68b4abac54f973edc7ff05046ba400c932bb12a8
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-11-01 10:11:52 -0400

    This commit updates the import path for the `REQUEST` token in the `ssr-cookie.service.ts` file. This is part of the upgrading process to Angular 17.0.0-rc.3 which required changes in import paths due to the restructuring of libraries and packages.

--- projects/ngx-cookie-service-ssr/src/lib/ssr-cookie.service.ts
+++ projects/ngx-cookie-service-ssr/src/lib/ssr-cookie.service.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import { REQUEST } from '@angular/ssr/tokens';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
 import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
