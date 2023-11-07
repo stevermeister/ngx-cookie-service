aeba35d57114bff7d0b252af6e441f0b17a48eb8
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-11-07 10:01:19 -0500

    build: switches from PNPM to NPM

--- projects/ngx-cookie-service-ssr/src/lib/ssr-cookie.service.ts
+++ projects/ngx-cookie-service-ssr/src/lib/ssr-cookie.service.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import {Inject, Injectable, InjectionToken, Optional, PLATFORM_ID} from '@angular/core';
+import { Inject, Injectable, InjectionToken, Optional, PLATFORM_ID } from '@angular/core';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 // Define the `Request` token
