eb2acf955d7e704b40aa1a830b158f5d045f2d8b
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2026-01-27 15:06:30 -0500

    feat(core): adds SSR demo

--- projects/ngx-cookie-service-ssr-demo/src/app/home/home.component.ts
+++ projects/ngx-cookie-service-ssr-demo/src/app/home/home.component.ts
@@ -1,37 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
-import { HomeService } from '../service/home.service';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
 import { MatButtonModule } from '@angular/material/button';
+import { httpResource } from '@angular/common/http';
+import { API_BASE_URL } from '../constants/app.constants';
+import { User } from '../model/user';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.component.html',
-	standalone: true,
-	imports: [MatButtonModule],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [MatButtonModule],
 })
 export class HomeComponent implements OnInit {
-	token: string | undefined;
-	userDisplayName: string | undefined;
+  authService = inject(AuthService);
+  cookieService = inject(SsrCookieService);
+  token: string | undefined;
+  user = httpResource<User>(() => `${API_BASE_URL}/home`);
 
-	constructor(
-		private authService: AuthService,
-		private homeService: HomeService,
-		private cookieService: SsrCookieService,
-	) {}
+  ngOnInit(): void {
+    this.token = this.cookieService.get('token');
+  }
 
-	ngOnInit(): void {
-		this.token = this.cookieService.get('token');
-		this.getUserInfo();
-	}
-
-	logout() {
-		this.authService.logout();
-	}
-
-	private getUserInfo() {
-		this.homeService.getUserInfo().subscribe((data) => {
-			this.userDisplayName = data.firstName + ' ' + data.lastName;
-		});
-	}
+  logout() {
+    this.authService.logout();
+  }
 }
--- projects/ngx-cookie-service-ssr-demo/src/app/login/login.component.ts
+++ projects/ngx-cookie-service-ssr-demo/src/app/login/login.component.ts
@@ -1,33 +1,48 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ReactiveFormsModule, UntypedFormBuilder, Validators} from '@angular/forms';
-import {AuthService} from '../service/auth.service';
-import {Router} from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { form, FormField, required } from '@angular/forms/signals';
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	standalone: true,
-	imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatFormFieldModule],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+  styles: [
+    `
+      .example-form {
+        min-width: 150px;
+        max-width: 500px;
+        width: 100%;
+      }
+
+      .example-full-width {
+        width: 100%;
+      }
+    `,
+  ],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatFormFieldModule, FormField],
 })
-export class LoginComponent implements OnInit {
-	fb = inject(UntypedFormBuilder);
-	router = inject(Router);
-	authService = inject(AuthService);
-	loginFormGroup = this.fb.group({
-		username: ['admin', Validators.required],
-		password: ['admin', Validators.required],
-	});
+export class LoginComponent {
+  router = inject(Router);
+  authService = inject(AuthService);
+  loginModel = signal({
+    username: 'admin',
+    password: 'admin',
+  });
+  loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.username, { message: 'Username is required' });
+    required(schemaPath.password, { message: 'Password is required' });
+  });
 
-	ngOnInit(): void {
-	}
-
-	loginUser() {
-		this.authService.login().subscribe((data) => {
-			console.log(data);
-			this.router.navigateByUrl('/home');
-		});
-	}
+  loginUser($event: Event) {
+    $event.preventDefault();
+    this.authService.login().subscribe((data) => {
+      console.log(data);
+      this.router.navigateByUrl('/home');
+    });
+  }
 }
